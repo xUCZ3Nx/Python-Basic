072430d0c2e732b9e2ada2e2c5964d55e3c6113d
Mateusz Kolodziej <mkolodziej98@tlen.pl>
2024-12-04 20:11:01 +0100

    Zadanko1

--- Zadanko1.py
+++ Zadanko1.py
@@ -2,8 +2,10 @@ import math
 class ProstokatWUkadzieKartezjanskim:
     def __init__(self, x1, x2, y1, y2):
         self.changexandy(x1, x2, y1, y2)
+    def __str__(self):
+        return f"x1={self.x1}, x2={self.x2}, y1={self.y1}, y2={self.y2}"
     def printall(self):
-        print(f"x1={self.x1}, x2={self.x2}, y1={self.y1}, y2={self.y2}")
+        return f"x1={self.x1}, x2={self.x2}, y1={self.y1}, y2={self.y2}"
     def Oblicz_pole_prostokata(self):
         return math.sqrt((self.x2-self.x1)**2)*math.sqrt((self.y2-self.y1)**2)
     def changexandy(self, x1, x2, y1, y2):
@@ -11,11 +13,32 @@ class ProstokatWUkadzieKartezjanskim:
         self.y1 = y1 
         self.x2 = x2 
         self.y2 = y2 
+    def Obwod(self):
+        return 2*((self.x2-self.x1)+(self.y2-self.y1))
+    def Czy_w_srodku(self, a, b):
+        amin = min(self.x1, self.x2)
+        amax = max(self.x1, self.x2)
+        bmin = min(self.y1, self.y2)
+        bmax = max(self.y1, self.y2)
+        if a > amin and a < amax and b > bmin and b < bmax:
+            print("Punkt znajduje sie w prostokacie")
+        else:
+            print("Punkt nie znajduje sie w prostokacie")
+    def wszystkie_informacje(self):
+        print(f"Polecenie 6\nWspolrzedne wierzcholkow: {x.printall()}")
+        x.printall() # nie zwraca bo jest return zamiast print
+        print("Pole prostokata:")
+        print(x.Oblicz_pole_prostokata())
+        print("Obwod prostokata:")
+        print(x.Obwod())
 
-
-x = ProstokatWUkadzieKartezjanskim(0,0,1,1)
-x.printall()
-print(x.Oblicz_pole_prostokata())
-x.changexandy(1, 1, 2, 2)
-x.printall()
-print(x.Oblicz_pole_prostokata())
+x = ProstokatWUkadzieKartezjanskim(0,1,0,1)
+# x.printall()
+# print(x.Oblicz_pole_prostokata())
+x.changexandy(1, 2, 1, 2) # 5. zmiana wymiarow
+# x.printall()
+# print(x.Oblicz_pole_prostokata())
+# print(x.Obwod())
+x.Czy_w_srodku(1.5, 1.5) # 4. Sprawdz czy punkt znajduje sie w srodku
+x.wszystkie_informacje() # 6. wszystkie informajce
+print(x)
